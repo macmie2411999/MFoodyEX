3eb366b40565da33c89280d2c0660f70991625e2
TênMac <caoducmanh2411999@gmail.com>
2023-01-09 23:46:15 +0300

    Set Up Project -byMacMie

---
+++ src/main/java/com/macmie/mfoodyex/MFoodyExApplication.java
@@ -0,0 +1,13 @@
+package com.macmie.mfoodyex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MFoodyExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MFoodyExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/macmie/mfoodyex/MFoodyExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macmie.mfoodyex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MFoodyExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
